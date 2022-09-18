5361a793967a8b862b3dad75f915359c6473ce52
ThulasiVasanth16 <108793655+ThulasiVasanth16@users.noreply.github.com>
2022-09-18 18:50:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+document.getElementsByClassName("article")[0].
+addEventListener("click", 
+function(){
+    window.open("https://thulasivasanth16.github.io/Grocery-List/")
+})
+
+document.getElementsByClassName("article")[1].
+addEventListener("click", 
+function(){
+    window.open("https://thulasivasanth16.github.io/To-Do-List/")
+})
+
+document.getElementsByClassName("article")[2].
+addEventListener("click", 
+function(){
+    window.open("https://thulasivasanth16.github.io/Digital-Clock/")
+})
+
+document.getElementsByClassName("fab fa-youtube")[0].
+addEventListener("click", 
+function(){
+    window.open("https://www.youtube.com/watch?v=pQN-pnXPaVg")
+})
+
+document.getElementsByClassName("fab fa-youtube")[1].
+addEventListener("click", 
+function(){
+    window.open("https://www.youtube.com/watch?v=yfoY53QXEnI")
+})
+
+document.getElementsByClassName("fab fa-youtube")[2].
+addEventListener("click", 
+function(){
+    window.open("https://www.youtube.com/watch?v=PkZNo7MFNFg")
+})
